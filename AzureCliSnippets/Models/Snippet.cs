f579317ff3c0ef3b75a1aea45b7587068b4d4fb2
Mark Heath <mark.heath@gmail.com>
2017-10-17 19:28:46 +0100

    adding a model and dbcontext

---
+++ AzureCliSnippets/Models/Snippet.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureCliSnippets.Models
+{
+    public class Snippet
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ AzureCliSnippets/Models/SnippetsContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureCliSnippets.Models
+{
+    public class SnippetsContext : DbContext
+    {
+        public SnippetsContext(DbContextOptions<SnippetsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Snippet> Snippets { get; set; }
+    }
+}
