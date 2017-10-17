2771777ff05ebfe78385a98fca3a72fb0ca7f0e8
Mark Heath <mark.heath@gmail.com>
2017-10-17 20:48:06 +0100

    page to trigger EF migration

---
+++ AzureCliSnippets/Pages/Migrate.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCliSnippets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureCliSnippets.Pages
+{
+    public class MigrateModel : PageModel
+    {
+        private readonly SnippetsContext _context;
+
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public MigrateModel(SnippetsContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            try
+            {
+                _context.Database.Migrate();
+                Message = "Migration Success";
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = $"Failed to migrate {e.Message}";
+            }
+        }
+    }
+}
